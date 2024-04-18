8d6ba438d25eac48b64653a5ad6c280a589d60d1
ipluchino <ianlikesmath@gmail.com>
2024-04-18 17:20:29 -0400

    Added to the project test, as well as cleaned up some code.

--- MLB Bet Buddy/ProjectTest.py
+++ MLB Bet Buddy/ProjectTest.py
@@ -1,4 +1,4 @@
-#Test file - to test all of the features and individual components of the bet predictor.
+#Project test file - to test all of the features and individual components of the bet predictor.
 
 from Endpoints import Endpoints
 from datetime import datetime
@@ -11,10 +11,10 @@ from LocalFactors import LocalFactors
 from BetPredictor import BetPredictor
 
 #Constants - will use 2023 for testing purposes.
-OPENING_DAY = datetime.strptime('03/30/2023', '%m/%d/%Y')
-CLOSING_DAY = datetime.strptime('10/01/2023', '%m/%d/%Y')
-SEASON = 2023
-DATE = datetime.strptime('06/15/2023', '%m/%d/%Y')
+OPENING_DAY = datetime.strptime('03/20/2024', '%m/%d/%Y')
+CLOSING_DAY = datetime.strptime('09/29/2024', '%m/%d/%Y')
+SEASON = 2024
+DATE = datetime.today()
 DATE_STRING = DATE.strftime('%m/%d/%Y')
 
 #Testing the Team class.
@@ -24,11 +24,11 @@ print('The name of the team is:', team.GetTeamName(), '\n')
 print('The team ID is:', team.GetTeamID(), '\n')
 print('Their record on', DATE_STRING, 'is:', team.GetRecord(DATE, SEASON), '\n')
 print('Their offensive statistics through', DATE_STRING, 'are:', team.GetTeamOffensiveStatistics(SEASON, OPENING_DAY, DATE), '\n')
-#print('Their YRFI percentage through', DATE_STRING, 'is', team.CalculateYRFIPercentage(SEASON, OPENING_DAY, DATE), '\n')
+print('Their YRFI percentage through', DATE_STRING, 'is', team.CalculateYRFIPercentage(SEASON, OPENING_DAY, DATE), '\n')
 
 #Testing the Game class.
 print('TESTING THE GAME CLASS')
-game = Game(718781)
+game = Game(746418)
 print('Information about the Yankees opening day game:')
 game.PrintGameInfo()
 
@@ -41,10 +41,10 @@ print('The hand information for Anthony Volpe is:', player.GetHandInformation(),
 
 #Testing the Pitcher class.
 print('TESTING THE PITCHER CLASS')
-pitcher = Pitcher(Player.FindPlayerID('Gerrit Cole'))
-print('Gerrit Cole\'s pitching statistics through', DATE_STRING, 'are:', pitcher.GetPitchingStatistics(SEASON, OPENING_DAY, DATE), '\n')
-print('Gerrit Cole\'s lefty/righty splits for the', SEASON, 'season are:', pitcher.GetLRPitchingSplits(SEASON), '\n')
-#print('Gerrit Cole\'s YRFI percentage through', DATE_STRING, 'is', pitcher.CalculateYRFIPercentage(SEASON, OPENING_DAY, DATE), '\n')
+pitcher = Pitcher(Player.FindPlayerID('Marcus Stroman'))
+print('Marcus Stroman\'s pitching statistics through', DATE_STRING, 'are:', pitcher.GetPitchingStatistics(SEASON, OPENING_DAY, DATE), '\n')
+print('Marcus Stroman\'s lefty/righty splits for the', SEASON, 'season are:', pitcher.GetLRPitchingSplits(SEASON), '\n')
+print('Marcus Stroman\'s YRFI percentage through', DATE_STRING, 'is', pitcher.CalculateYRFIPercentage(SEASON, OPENING_DAY, DATE), '\n')
 
 #Testing the Hitter class.
 print('TESTING THE HITTER CLASS')
@@ -52,8 +52,8 @@ hitter = Hitter(Player.FindPlayerID('Austin Hays'))
 print('Austin Hays\' offensive statistics through', DATE_STRING, 'are:', hitter.GetOffensiveStatistics(SEASON, OPENING_DAY, DATE), '\n')
 
 careerStats = hitter.GetCareerStatsOffPitcher(pitcher.GetPlayerID())
-print('Austin Hays\' career stats against Gerrit Cole are:', careerStats, '\n')
-print('Austin Hays is \"', hitter.ClassifyHitting(careerStats['battingAverage'])[0], '\" based on his career stats against Gerrit Cole.', '\n')
+print('Austin Hays\' career stats against Marcus Stroman are:', careerStats, '\n')
+print('Austin Hays is \"', hitter.ClassifyHitting(careerStats['battingAverage'])[0], '\" based on his career stats against Marcus Stroman.', '\n')
 
 L10Stats = hitter.Last10Stats(SEASON, DATE)
 print('Austin Hays\' last 10 games stats starting from', DATE_STRING, 'are:', L10Stats, '\n')
@@ -61,7 +61,7 @@ print('Austin Hays is \"', hitter.ClassifyHitting(L10Stats['battingAverage'])[0]
 print('Austin Hays\' lefty/righty splits for the', SEASON, 'season are:', hitter.GetLRHittingSplits(SEASON), '\n')
 
 qualifiedHitters = Hitter.GetAllHitters(SEASON)
-print('In total, there were', len(qualifiedHitters), 'qualified hitters in the', SEASON, 'season.', '\n')
+print('In total, there are', len(qualifiedHitters), 'qualified hitters in the', SEASON, 'season.', '\n')
 
 #Testing the LocalFactors class.
 print('TESTING THE LOCALFACTORS CLASS')
@@ -74,21 +74,31 @@ print('The city that', stadium, 'is in is:', localFactors.GetCityForStadium(stad
 print('Does', stadium, 'have a roof?', localFactors.HasRoof(stadium), '\n')
 
 localTime = '1:00 PM'
-#print('The weather information at', localTime, 'at', stadium, 'is:', localFactors.GetWeather(stadium, localTime), '\n')
+print('The weather information at', localTime, 'at', stadium, 'is:', localFactors.GetWeather(stadium, localTime), '\n')
 
 #Testing the BetPredictor class.
-'''
-date = datetime.strptime('03/29/2024', '%m/%d/%Y')
-OPENING_DAY = datetime.strptime('03/20/2024', '%m/%d/%Y')
-
+print('TESTING THE BETPREDICTOR CLASS', '\n')
 bp = BetPredictor()
 
-schedule = bp.CreateSchedule(date, 2024)
-schedule.to_excel('schedule_' + date.strftime('%m-%d-%Y') +'.xlsx')
-
-NRFI = bp.CreateNRFIPredictions(schedule, OPENING_DAY, 2024)
-NRFI.to_excel('NRFI_' + date.strftime('%m-%d-%Y') +'.xlsx')
-    
-hitting = bp.CreateHittingPredictions(schedule, OPENING_DAY, date, 2024)
-hitting.to_excel('hitting_' + date.strftime('%m-%d-%Y') +'.xlsx')
 '''
+schedule = bp.CreateSchedule(DATE, 2024)
+print('SCHEDULE FOR', DATE_STRING + ':')
+print(schedule)
+
+NRFIYRFI = bp.CreateNRFIPredictions(schedule, OPENING_DAY, 2024)
+print('NRFI/YRFI BET PREDICTIONS FOR', DATE_STRING + ':')
+print(NRFIYRFI)
+
+hitting = bp.CreateHittingPredictions(schedule, OPENING_DAY, DATE, 2024)
+print('HITTING BET PREDICTIONS FOR', DATE_STRING + ':')
+print(hitting)
+'''
+
+print('TESTING THE ACCURACY OF BET PREDICTIONS FOR GAME DATA FROM 2022-2023')
+NRFIYRFIAccuracy = bp.AccuracyTestNRFIYRFI(1)
+print('NRFI/YRFI ACCURACY:', NRFIYRFIAccuracy, '\n')
+
+hittingAccuracy = bp.AccuracyTestHitting(3)
+print('HITTING ACCURACY:', hittingAccuracy, '\n')
+
+
